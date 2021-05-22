3c0d3f3f93ca2b911d5b35f87090ae2723bc33f2
Sergey-Kupchik <kupchikrabota@gmail.com>
2021-05-22 14:46:16 -0400

    More

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -14,3 +14,30 @@ test('renders non-empty component without crashing', () => {
   const warpper = shallow(<App/>)
  expect(warpper.exists()).toBe(true)
 });
+
+
+test('renders without error', () => {
+  const warpper = shallow(<App/>)
+  const appComponent = warpper.find('[data-test="component-app"]')
+  expect(appComponent.length).toBe(1)
+});
+
+test('renders increment button', () => {
+  const warpper = shallow(<App/>)
+
+});
+
+test('renders counter dispay', () => {
+    const warpper = shallow(<App/>)
+  
+  });
+
+  test('counter dispay starts at 0', () => {
+    const warpper = shallow(<App/>)
+  
+  });
+
+  test('clicking button increments counter dispay', () => {
+    const warpper = shallow(<App/>)
+  
+  });
